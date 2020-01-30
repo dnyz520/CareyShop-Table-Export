e3d0d0c2a0aca57902310378d0a639e20a9e07e0
思维空间 <dnyz520@126.com>
2020-01-30 11:46:19 +0800

    up

--- tableExport.js
+++ tableExport.js
@@ -1,14 +1,13 @@
 import exportFromJSON from "export-from-json"
 
 export class tableExport {
-  constructor(opts = {
-    columns: [],
-    data: [],
-    fileName: 'export',
-    type: 'xls',
-    withBOM: false,
-  }) {
-    this.defOpts = opts
+  constructor(columns = [], data = [], replace = {}, fileName = 'export', type = 'xls', withBOM = false) {
+    this.columns = columns;
+    this.data = data;
+    this.replace = replace;
+    this.fileName = fileName;
+    this.type = type;
+    this.withBOM = withBOM;
   }
 
   /**
@@ -17,10 +16,10 @@ export class tableExport {
    */
   dataHandler() {
     let outputData = [];
-    this.defOpts.data.map((value) => {
+    this.data.map((value) => {
       let obj = {};
-      this.defOpts.columns.forEach(element => {
-        obj[element.label] = value[element.prop]
+      this.columns.forEach(element => {
+        obj[element.label] = this.dataReplace(element.prop, value);
       });
 
       outputData.push(obj);
@@ -30,25 +29,34 @@ export class tableExport {
   }
 
   /**
-   * 导出text, json, csv, xls文件
-   * @param {Object} opts
-   * @param {String} opts.fileName 文件名
-   * @param {String} opts.type 输出格式 默认为xls文件
-   * @param {String} opts.withBOM 将BOM（字节顺序标记）元添加到CSV文件。 Excel在读取UTF8 CSV文件时需要BOM。默认为false。
+   * 数据替换
+   * replace = {key: {value1: data, value2: data}}
    */
-  export (opts = {}) {
+  dataReplace(key, value) {
+    if (!this.replace.hasOwnProperty(key)) {
+      return value[key];
+    }
+
+    let val = value[key];
+    let valueMap = this.replace[key];
+
+    if (valueMap.hasOwnProperty(val)) {
+      return valueMap[val];
+    }
+
+    return val;
+  }
+
+  /**
+   * 导出text, json, csv, xls文件
+   */
+  export() {
     try {
-      let data = this.dataHandler();
-
-      function hasProperty(target, name, defVal) {
-        return target.hasOwnProperty(name) ? target[name] : defVal
-      }
-
       exportFromJSON({
-        data: data,
-        fileName: hasProperty(opts, 'fileName', this.defOpts.fileName),
-        exportType: hasProperty(opts, 'type', this.defOpts.type),
-        withBOM: hasProperty(opts, 'withBOM', this.defOpts.withBOM)
+        data: this.dataHandler(),
+        fileName: this.fileName,
+        exportType: this.type,
+        withBOM: this.withBOM
       });
     } catch (error) {
       console.error(error);
